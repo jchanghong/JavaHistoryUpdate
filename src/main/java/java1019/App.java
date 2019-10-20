d747d906cd924e747d9b7d942cb8148a368f617a
jiang <3200601@qq.com>
2019-10-20 20:25:21 +0800

    ini

---
+++ src/main/java/java1019/App.java
@@ -0,0 +1,21 @@
+package java1019;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+  public static void main(String[] args) {
+      System.out.println("""
+                   ||||
+            ||| =hello world= |||
+      """);
+  }
+}
